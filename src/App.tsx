a708ee13cd84d1d24929de5c3988f7f9f25d4829
theomaro <theo.pombe@gmail.com>
2025-04-23 19:32:24 +0300

    define basic pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import ResumeBuiler from "./pages/ResumeBuiler";
+import Templates from "./pages/Templates";
+import HowItWorks from "./pages/HowItWorks";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <h1 className="font-semibold p-2 text-lg text-rose-700">Vite + React</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+
+        <Route path="resume" element={<ResumeBuiler />} />
+
+        <Route path="templates" element={<Templates />} />
+        <Route path="how-it-works" element={<HowItWorks />} />
+        <Route path="contact" element={<Contact />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/ResumeBuiler.tsx
@@ -0,0 +1,5 @@
+const ResumeBuiler = () => {
+  return <div>ResumeBuiler</div>;
+};
+
+export default ResumeBuiler;
