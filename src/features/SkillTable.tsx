d8b519da76a57e4434c9681d8a7a4b13b3735940
theomaro <theo.pombe@gmail.com>
2025-04-28 03:53:08 +0300

    add skill table

---
+++ src/features/SkillTable.tsx
@@ -0,0 +1,59 @@
+import { Edit, Eye, X } from "lucide-react";
+
+const SkillTable = ({ skills }: { skills: Skill[] }) => {
+  return (
+    <div className="overflow-x-auto pb-5">
+      <table className="w-full text-sm text-left text-gray-500">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+          <tr>
+            <th className="py-4">Skill Name</th>
+            <th className="py-4">Proficiency (%)</th>
+            <th className="py-4">Certification</th>
+            <th className="py-4 sr-only">Action</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {skills.length ? (
+            skills.map((skill) => (
+              <tr
+                key={skill._id}
+                className="odd:bg-white even:bg-gray-50 border-b"
+              >
+                <td className="py-4 text-nowrap">{skill.name}</td>
+                <td className="py-4 text-nowrap">{skill.proficiency}</td>
+                <td className="py-4 text-nowrap">
+                  <button
+                    title="preview"
+                    className="flex space-x-2 cursor-pointer"
+                  >
+                    <Eye size={14} />
+                    <span className="text-xs underline decoration-dotted">
+                      Certificate
+                    </span>
+                  </button>
+                </td>
+                <td className="py-4 text-nowrap">
+                  <div className="flex items-center space-x-5">
+                    <button title="edit" className="cursor-pointer">
+                      <Edit size={14} />
+                    </button>
+                    <button title="delete" className="cursor-pointer">
+                      <X size={14} />
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td className="px-6 py-4 text-rose-700">No skill is added yet</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default SkillTable;
